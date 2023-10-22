f5b0146bb008a6f96400ab1dcf7ea7193e827474
vale <valentin.necula02@gmail.com>
2023-10-22 17:47:34 +0200

    feat: added comments

--- main.go
+++ main.go
@@ -12,34 +12,33 @@ import (
 	"time"
 )
 
-const maxPasswordLength = 5 //the max password length (more than 5 will take too long)
+const maxPwdLength = 5 //the max password length (more than 5 will take too long)
 
-//this function decrypts the data with the password
-//
-
-func decrypt(data []byte, password []byte) bool { //function to decrypt the data with the password
-	decoder := bytes.NewBuffer(data) //create a buffer with the data
+// A PromptFunction is used as a callback by functions that may need to decrypt
+// a private key, or prompt for a passphrase. If the decrypted private key or given passphrase isn't
+// correct, the function will be called again, forever. That's why i have the flag called.
+func decrypt(data []byte, pwd []byte) bool { //function to decrypt the data with the pwd
+	buffer := bytes.NewBuffer(data) //create a buffer with the data
 
 	called := false                                                      //variable to check if the prompt function was called
-	prompt := func(keys []openpgp.Key, symmetric bool) ([]byte, error) { //function to prompt for the password
+	prompt := func(keys []openpgp.Key, symmetric bool) ([]byte, error) { //function to prompt for the pwd
 		if called {
-			return nil, fmt.Errorf("single attempt made")
+			return nil, fmt.Errorf("one attmept only")
 		}
 		called = true
-
-		return password, nil
+		return pwd, nil
 	}
 
-	_, err := openpgp.ReadMessage(decoder, nil, prompt, nil) //try to decrypt the data, this function
+	_, err := openpgp.ReadMessage(buffer, nil, prompt, nil) //try to decrypt the data, this function
 	isDecryptError := err != nil
 	return !isDecryptError
 }
 
-func worker(data []byte, ch <-chan string, wg *sync.WaitGroup, resultCh chan<- string) {
-	defer wg.Done()
-	for password := range ch {
-		if decrypt(data, []byte(password)) {
-			resultCh <- password
+func worker(data []byte, wg *sync.WaitGroup, pwdCh <-chan string, resCh chan<- string) {
+	defer wg.Done() //when the function finishes, put the worker in done
+	for pwd := range pwdCh {
+		if decrypt(data, []byte(pwd)) {
+			resCh <- pwd
 			return
 		}
 	}
@@ -52,73 +51,72 @@ func worker(data []byte, ch <-chan string, wg *sync.WaitGroup, resultCh chan<- s
 // it stops when it reaches the max password length
 // it sends the passwords to the channel
 // it closes the channel when it finishes
-func generatePasswords(ch chan<- string) {
+func pwdGenerator(ch chan<- string) {
 	const chars = "abcdefghijklmnopqrstuvwxyz"   //the characters to use in the passwords (ascii lowercase letters, from the challenge)
 	var generate func([]byte, int)               //the function to generate the passwords
 	generate = func(prefix []byte, length int) { //the function to generate the passwords
 		if length == 0 {
-			password := string(prefix)
-
-			ch <- password
+			pwd := string(prefix)
+			ch <- pwd
 			return
 		}
-		for _, c := range chars { //for each character
+		for _, c := range chars {
 			newPrefix := append(append([]byte{}, prefix...), byte(c)) //append the character to the prefix
 			generate(newPrefix, length-1)                             //call the function with the new prefix and the length - 1
 		}
 	}
-	for length := 1; length <= maxPasswordLength; length++ { //for each length
-		generate([]byte{}, length)
+	for length := 1; length <= maxPwdLength; length++ { //loop from 1 to the max password length
+		generate([]byte{}, length) //call the function with an empty prefix and the length
 	}
 	close(ch) //close the channel
 }
 
 func main() {
-	startTime := time.Now() // Capture start time
+	start := time.Now()
 
-	debug.SetGCPercent(-1) // Disable GC to gain performance
+	debug.SetGCPercent(-1) // Disable Garbage Collector for performance improvements
 
-	if len(os.Args) < 2 { //check if there is an argument
-		log.Fatal("Please provide a file to decrypt") //if there is no argument, the program will exit
+	if len(os.Args) < 2 { //file path as argument
+		log.Fatal("Provide the path of the file to decrypt")
 	}
 
-	encryptedFilePath := os.Args[1]             //get the file path from the console
-	data, err := os.ReadFile(encryptedFilePath) //read the file
-	if err != nil {                             //check if there is an error
-		log.Fatal("Error reading file: ", err)
+	path := os.Args[1]             //get the file path from the console
+	data, err := os.ReadFile(path) //read the file
+	if err != nil {                //check for errors
+		log.Fatal("Error while parsing the file: ", err)
 	}
 
 	//get the number of cores
 	//not using more because it uses IO, and having more workers than cores will not increase performance
-	workerCount := runtime.NumCPU()
+	workers := runtime.NumCPU()
 
 	//create a channel with a buffer of 1000 to store the passwords
 	//this ensures that the workers will not wait for the password generator to generate a password
 	//this also ensures that the password generator will not wait for the workers to finish
 	//no need to make it bigger cause the decryption takes more time than the password generation
-	ch := make(chan string, 1000)
+	pwdCh := make(chan string, 1000)
 
 	//create a channel with a buffer of 1 where the workers will send the result
-	resultCh := make(chan string, 1)
+	resCh := make(chan string, 1)
 
 	//create a wait group to wait for the workers to finish
 	var wg sync.WaitGroup
 
 	//start the workers and add them to the wait group
 	//this ensures that the program will not exit before the workers finish
-	for i := 0; i < workerCount; i++ {
+	for i := 0; i < workers; i++ {
 		wg.Add(1)
-		go worker(data, ch, &wg, resultCh)
+		go worker(data, &wg, pwdCh, resCh)
 	}
 
 	//start the password generator
 	//no need to add it to the wait group because if the password is found before the generator finishes, the program needs to exit
-	go generatePasswords(ch)
+	go pwdGenerator(pwdCh)
 
 	//select statement to start the goroutine to wait for the wait group to finish
 	select {
-	case password := <-resultCh: //wait for the result
-		fmt.Println("Password found:", password)
+	case pwd := <-resCh: //wait for the result
+		fmt.Println("Password found:", pwd)
 	case <-func() chan struct{} { //wait for the wait group to finish
 		ch := make(chan struct{}) //create a channel to return
 		go func() {               //start a goroutine to wait for the wait group to finish
@@ -130,6 +128,6 @@ func main() {
 		fmt.Println("No password found")
 	}
 
-	elapsedTime := time.Since(startTime)                // Capture end time
-	fmt.Printf("Program executed in %s\n", elapsedTime) // Print execution time
+	endTime := time.Since(start)
+	fmt.Printf("Time to execute:  %s\n", endTime)
 }
